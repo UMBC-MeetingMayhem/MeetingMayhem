67afcaf517c711e33e6c995bf9d61b3ba7d37dcc
Rob Shovan <83430825+Voitheia@users.noreply.github.com>
2021-09-13 18:56:04 -0400

    Update models.py
    talked to hamza about the fields we need, we shouldn't need actual fields for the keys because we can assume if its signed, then we know the sender signed it and if its encrypted, then it gets encrypted with the recipients public keys.

--- MeetingMayhem/models.py
+++ MeetingMayhem/models.py
@@ -93,10 +93,7 @@ class Message(db.Model):
     is_deleted = db.Column(db.Boolean, nullable=False) #keeps track if the adversary "deleted" the message
     adv_created = db.Column(db.Boolean, nullable=False) #keeps track if the adversary made this message
     is_encrypted = db.Column(db.Boolean, nullable=False, default=False) #keeps track of whether the message is encrypted or not
-    encryption_key = db.Column(db.String(41), nullable=False, default='null') #keeps track of how the message was encrypted if encrypted (format will be <username>.<pub>)
-    is_signed = db.Column(db.Boolean, nullable=False, default=False) #keeps track of whether the message is encrypted or not
-    signature_key = db.Column(db.String(41), nullable=False, default='null') # keeps track of how the message was signed if signed (format will be <username>.<priv>)
-    
+    is_signed = db.Column(db.Boolean, nullable=False, default=False) #keeps track of whether the message is signed or not
     
     def __repr__(self): #this is what gets printed out for the message, just spits out everything
         return f"Message(ID='{self.id}', Round='{self.round}', Game='{self.game}' Sender='{self.sender}', Recipient='{self.recipient}', Content='{self.content}', Edited='{self.is_edited}', New Sender='{self.new_sender}', New Recipient='{self.new_recipient}', New Content='{self.edited_content}', Deleted='{self.is_deleted}', Adv Created='{self.adv_created}')\n"
