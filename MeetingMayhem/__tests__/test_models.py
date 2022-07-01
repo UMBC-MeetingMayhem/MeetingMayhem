7953dee21d0a292c0f9d68195c3465b0ee6ae6e3
Drew Barlow <drew.barlow@umbc.edu>
2022-07-01 15:45:34 -0400

    removed -t from run.py, added shell script for testing, failing model test

--- MeetingMayhem/__tests__/test_models.py
+++ MeetingMayhem/__tests__/test_models.py
@@ -3,6 +3,21 @@ Testing Framework: flask_unittest
     -Unfamiliar with pytest
     -Unittest DB mocks seem awful
 """
-# import flask_unittest
+import flask_unittest as test
 import MeetingMayhem.models as MMmodel
 
+class Ex(test.ClientTestCase):
+    app = None  # app obj goes here when ready
+
+    def setUp(self, client) -> None:
+        return
+
+    def tearDown(self, client) -> None:
+        return
+
+    def test_basic_assertion(self, client) -> None:
+        self.assertEqual(1, 1)
+        self.assertTrue(1 == 1)
+        self.assertFalse(1, 2)
+
+        return
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 -m unittest MeetingMayhem/__tests__/*
