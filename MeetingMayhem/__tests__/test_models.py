a47d0c8b861fe345bf630bf7528a43bb1ae2e2a4
Drew Barlow <drew.barlow@umbc.edu>
2022-07-07 10:39:47 -0400

    finished model tests

--- MeetingMayhem/__tests__/test_models.py
+++ MeetingMayhem/__tests__/test_models.py
@@ -1,11 +1,12 @@
 import flask_unittest as test
 import MeetingMayhem.models as MMmodel
 from flask import Flask
+from flask_sqlalchemy import BaseQuery
 from MeetingMayhem import app as MMapp
 from MeetingMayhem import db
 from random import choices
 from string import ascii_letters, digits
-from typing import List
+from typing import List, Tuple
 
 # I REALLY hate the idea of using the same DB for testing and production.
 # I'm sure it'll be fine... right?? -Drew
@@ -29,6 +30,9 @@ def insert_user(role: int) -> MMmodel.User:
 class UserTests(test.ClientTestCase):
     app: Flask = MMapp
     inserted: List[MMmodel.User] = []
+    base_num_users: int = MMmodel.User.query.filter_by(role=4).count()
+    base_num_adv: int = MMmodel.User.query.filter_by(role=3).count()
+    base_num_gm: int = MMmodel.User.query.filter_by(role=2).count()
 
     def setUp(self, client) -> None:
         user1: MMmodel.User = insert_user(4)
@@ -52,6 +56,10 @@ class UserTests(test.ClientTestCase):
         Adversaries have role: int = 3.
         getAdversary() should return all users with this role.
         """
+        result: List[MMmodel.User] = list(MMmodel.getAdversary())
+        diff: int = len(result) - self.base_num_adv
+        self.assertEqual(diff, 1)
+        self.assertTrue(self.inserted[2] in result)
 
         return
 
@@ -60,6 +68,10 @@ class UserTests(test.ClientTestCase):
         Normal users have role: int = 4, Adversaries have role: int = 3.
         getAllUserAdversary() should return all users with these roles.
         """
+        result: BaseQuery = MMmodel.getAllUserAdversary()
+        diff: int = result.count() - (self.base_num_users + self.base_num_adv)
+        self.assertEqual(diff, 3)
+        self.assertTrue(all([self.inserted[i] in result for i in range(2)]))
 
         return
 
@@ -68,6 +80,10 @@ class UserTests(test.ClientTestCase):
         Game Masters have role: int = 2 (I believe).
         getNonGMUsers() should return all users with role: int > 2.
         """
+        result: BaseQuery = MMmodel.getNonGMUsers()
+        diff: int = result.count() - (self.base_num_users + self.base_num_adv)
+        self.assertEqual(diff, 3)
+        for user in self.inserted: self.AssertIn(user, result)
 
         return
 
@@ -76,22 +92,50 @@ class UserTests(test.ClientTestCase):
 # with dataclasses.dataclass. -Drew
 # https://python.plainenglish.io/why-you-should-start-using-pythons-dataclasses-cd6a73ae5614
 class MessageTests(test.ClientTestCase):
-    app = MMapp
+    app: Flask = MMapp
 
     def setUp(self, client) -> None:
         return
 
     def tearDown(self, client) -> None:
         return
+
+# See insert_user() for me complaining. -Drew
+def create_game(*, running: bool) -> Tuple[MMmodel.Game, MMmodel.User]:
+    adv: MMmodel.User = insert_user(3)
+    game: MMmodel.Game = MMmodel.Game(
+        name=f"{'' if running else 'Not '}Running Game",
+        is_running=running,
+        adversary=adv.username,
+        players="",
+        current_round=1,
+        adv_current_msg=1,
+        adv_current_msg_list_size=1
+    )
+    db.session.add(game)
+    db.session.commit()
+
+    return game, adv
 
 # See the UserTests comment. -Drew
 class GameTests(test.ClientTestCase):
-    app = MMapp
+    app: Flask = MMapp
+    inserted: List[MMmodel.Game] = []
+    base_num_running: int = MMmodel.Game.query.filter_by(is_running=True).count()
 
     def setUp(self, client) -> None:
+        running_details: Tuple[MMmodel.Game, MMmodel.User] = create_game(running=True)
+        not_running_details: Tuple[MMmodel.Game, MMmodel.User] = create_game(running=False)
+
+        self.inserted = [*running_details, *not_running_details]
         return
 
     def tearDown(self, client) -> None:
+        for item in self.inserted:
+            db.session.delete(item)
+
+        db.session.commit()
+        self.inserted = []
         return
 
     def test_get_game(self, client) -> None:
@@ -99,6 +143,11 @@ class GameTests(test.ClientTestCase):
         Games have an is_running field.
         getGame() should only return games that are currently in progress.
         """
+        result: List[MMmodel.Game] = list(MMmodel.getGame())
+        diff: int = len(result) - self.base_num_running
+        self.assertEqual(diff, 1)
+        self.assertTrue(self.inserted[0] in result)
+        self.assertTrue(self.inserted[2] not in result)
 
         return
 
